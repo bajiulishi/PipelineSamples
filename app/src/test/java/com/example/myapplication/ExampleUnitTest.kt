bc38d1e7bcec8f2107ff704750eca259ebdf91d5
yaguang <yaguang_li@epam.com>
2023-07-28 16:40:28 +0800

    update ut

--- app/src/test/java/com/example/myapplication/ExampleUnitTest.kt
+++ app/src/test/java/com/example/myapplication/ExampleUnitTest.kt
@@ -1,5 +1,7 @@
 package com.example.myapplication
 
+import org.junit.Assert.assertEquals
+import org.junit.Test
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -7,5 +9,8 @@ package com.example.myapplication
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
-
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
 }
